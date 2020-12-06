90ea2c5673c63fb146e1ba576cad0815bf9654ac
Girish Ns <nsgirish@gmail.com>
2020-12-07 01:47:57 +0800

    Update https_json_post.groovy

--- https_json_post.groovy
+++ https_json_post.groovy
@@ -24,6 +24,50 @@ def jsonMessage = jsonMessage.value
 try
 {
   flowFile = session.write(flowFile,{inputStream,outputStream -> 
+    //setup keystore
+    def clientStore = KeyStore.getInstance(keyStoreType)//i
+    clientStore.load(new FileInputStream(keyStoreFilename),keyStorePassword.toCharArray())
+    def kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+    def kms = kmf.getKeyManagers()
+    
+    //setup truststore
+    def trustStore = KeyStore.getInstance(trustStoreType)
+    trustStore.load(new FileInputStream(trustStoreFilename),trustStorePassword.toCharArray())
+    def tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+    tmf.init(trustStore)
+    def tms = tmf.getTrustManagers()
+    
+    //init ssl context
+    def sslContext = SSLContext.getInstance(tlsProtocol)
+    sslContext.init(kms,tms,null)
+    
+    //init https connection
+    def conn = (HttpsURLConnection)new URL(url).openConnection()
+    
+    //disble host name verification in client side (optional)
+    conn.setHostnameVerifier(new HostnameVerifier() {
+        @override
+        public boolean verify(String s,SSLSession sslSession) 
+        {
+          return true
+        }
+    }
+    //init ssl factory
+    conn.setSSLSocketFactory(sslContext.getSocketFactory())
+                      
+    //generate https request
+    conn.setRequestMethod("POST")
+    conn.setDoOutput(true)
+    conn.setRequestProperty("Content-Type","application/json")
+    conn.getOutputStream().write(jsonMessage.getBytes("UTF-8")
+                                 
+    //process https response
+    def responseCode = conn.getResponseCode()
+    def responseBody = IOUtils.toString(conn.getInputStream(),StandardCharsets.UTF_8)
+                                 
+    //Write response to flowfile
+    outputStream.write(responseBody.getBytes(StandardCharsets.UTF_8)
+                              
   } as StreamCallback
   
   //transfer control to success 
