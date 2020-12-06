41a17cd588e1607e8088219c71ab6ec94419aca6
Girish Ns <nsgirish@gmail.com>
2020-12-07 01:25:10 +0800

    Update https_json_post.groovy

--- https_json_post.groovy
+++ https_json_post.groovy
@@ -9,7 +9,18 @@ import java.security.KeyStore
 def flowFile = session.get()
 if(!flowFile) return
 
-//init https post json parameters from dynamically added attributes of ExecuteScript processor
+//init parameters for https json post request from dynamucally added attributes of NIFI ExecuteScript processor 
 def keyStoreFilename = keyStoreFilename.value
+def keyStorePassword = keyStorePassword.value
+def keyPassword = keyPassword.value
+def keyStoreType = keyStoreType.value
+def trustStoreFilename = trustStoreFilename.value
+def trsutStorePassword = trustStorePassword.value
+def trustStoreType = trustStoreType.value
+def tlsProtocol = tlsProtocol.value
+def url = httpsUrl.value
+def jsonMessage = jsonMessage.value
+
+
 
 
